dbfbb450423208dfed28931da205ca012380ba0a
Ady Levy <ady.levy@teamaol.com>
2017-02-15 23:01:11 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ var nconf = require('nconf');
 nconf.argv().env();
 
 nconf.defaults({
-  'MONGOLAB_URI': 'mongodb://heroku_sbj5wl8m:heroku_sbj5wl8m@ds153669.mlab.com:53669/heroku_sbj5wl8m',
+  'MONGOLAB_URI': 'mongodb://heroku_app32308261:lg7149q2tg5slot9g8vpupllq5@ds029051.mongolab.com:29051/heroku_app32308261',
 	'PORT': 53669,
 	'GITHUB_CLIENT_ID': 'REPLACE_W_GITHUB_CLIENT_ID',
 	'GITHUB_CLIENT_SECRET': 'REPLACE_W_GITHUB_CLIENT_SECRET',
